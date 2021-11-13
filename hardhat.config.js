533d06e7e014f88c59faa6707330313b422f5c66
0xyyyy <ymoxley@gmail.com>
2021-11-13 15:12:41 -0500

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -35,6 +35,7 @@ require("./tasks/deploy_berries_rewards");
 require("./tasks/deploy_berries_single");
 require("./tasks/set_stacked_staked_rewards");
 require("./tasks/deploy_default_erc20");
+require("./tasks/deploy_lottery");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
