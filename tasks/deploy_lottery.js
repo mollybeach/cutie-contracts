ab23b392c877b7ce81c5306892f4e7e4e82679e3
0xyyyy <ymoxley@gmail.com>
2021-11-13 13:54:50 -0500

    deploy lottery

---
+++ tasks/deploy_lottery.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const deployments = require('../data/deployments');
+
+task('deploy-lottery').setAction(async function () {
+  const [deployer] = await ethers.getSigners();
+
+  const stackAddress = deployments.defaultErc20
+
+  const factory = await ethers.getContractFactory('Lottery', deployer);
+
+  const instance = await factory.deploy(
+    stackAddress
+  );
+
+  await instance.deployed();
+
+  console.log(`Deployed Lottery to: ${instance.address}`);
+  deployments.lottery = instance.address;
+
+  const json = JSON.stringify(deployments, null, 2);
+  fs.writeFileSync(`${__dirname}/../data/deployments.json`, `${json}\n`, {
+    flag: 'w',
+  });
+});
