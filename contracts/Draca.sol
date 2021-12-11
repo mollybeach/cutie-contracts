12e7de75d502a4c33635f74a73cd5de1d0f792ca
mollybeach <mollyjbeach@gmail.com>
2021-12-10 14:23:34 -1000

    Completed draca contract and tests

--- contracts/Draca.sol
+++ contracts/Draca.sol
@@ -112,7 +112,7 @@ contract Draca is ERC721Enumerable, Ownable {
         require(started, "not started");
         require(_qty > 0 , "need to mint at least 1 NFT");
         require(freeTotal + _qty <= freeSupply, "This mint would pass max freesupply");
-        require(genesisAddress.balanceOf(msg.sender) < maxMintsPerWallet, "Must be a holder of Genesis Token to receive a free mint of Draca");
+        require(genesisAddress.balanceOf(msg.sender) > 1,  "Must be a holder of Genesis Token to receive a free mint of Draca");
         require(IERC721(contractAddress).balanceOf(msg.sender) < maxMintsPerWallet, "Max mint amount allowed exceeded for this wallet for free mints");
         _mint(msg.sender, freeTotal + _qty);
         mint(_qty);
